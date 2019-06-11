38cafff9076e2afd1e30867b9a12660c72cd38fb
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-11 11:21:03 -0700

    created middleware that checks if user has a session and a username

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,25 +1,9 @@
-const bcrypt = require('bcryptjs');
 
-const Users = require('../users/users-model.js');
-// middleware to read username and password from headers and verify them
+
 module.exports = function restricted(req, res, next) {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'You shall not pass!' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
+  if (req.session && req.session.username) {
+    next();
   } else {
-    res.status(400).json({message: 'Please provide credentials'});
+  res.status(401).json({ message: 'You shall not pass!' });
   }
-   
 };
