19c74c5526dd04e661df1c1f16a81ba832c3049d
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-11 11:23:06 -0700

    added user to sessions in post /login router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -33,6 +33,7 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.username = user.username;
         res.status(200).json({ message: `You are logged in ${user.username}!` });
       } else {
         res.status(401).json({ message: 'You shall not pass!' });
